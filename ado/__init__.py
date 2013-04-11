c6911c356e4f90dd346755362f4a0f60bc8b8f24
Ana Nelson <ana@ananelson.com>
2013-04-11 09:17:48 -0700

    Move ado report to 'adoreport'. Revert mistaken changes to MANIFEST.in

--- ado/__init__.py
+++
@@ -1,7 +0,0 @@
-from dexy.template import Template
-
-class Report(Template):
-    """
-    HTML report using d3.js
-    """
-    ALIASES = ['report']
--- setup.py
+++ setup.py
@@ -7,8 +7,9 @@ setup(
         packages=find_packages(),
         version=ADO_VERSION,
         install_requires = [
-            'python-modargs',
-            'dexy>0.9.9'
+            'python-modargs>=1.7',
+            'Markdown', # for reports
+            'dexy>=0.9.9'
             ],
         entry_points = {
             'console_scripts' : [ 'ado = ado.commands:run' ]
